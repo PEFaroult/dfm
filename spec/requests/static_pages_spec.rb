e18677022fddd71559816f7be196e3a5045f6cf9
pefaroult <pefaroult@gmail.com>
2013-11-14 17:14:10 +0100

    Define full_title helper

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -3,12 +3,11 @@ require 'spec_helper'
 describe "StaticPages" do
   
   describe "Home" do
+	subject { page }
+  	before { visit root_path }
 
-  	before{ visit root_path }
-
-    it { should have_content('Du fait maison') }    
-    it { should have_title(full_title('')) }    
-    it { should_not have_title('| Bienvenue') }
+    it { should have_selector('h1', text: 'Bienvenue') }    
+    it { should have_title(full_title('Bienvenue')) }    
    end
 
 end
